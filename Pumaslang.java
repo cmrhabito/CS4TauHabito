af4f66afc3067fd4420c5a2f1a11f0716a5c9257
cmrhabito <97026339+cmrhabito@users.noreply.github.com>
2022-09-07 08:54:56 +0800

    Add files via upload

---
+++ Pumaslang.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pumaslang;
+
+/**
+ *
+ * @author TAU
+ */
+public class Pumaslang {
+
+    /**
+     * @param args the command line arguments
+     */
+  public static void main(String[] args) {
+      
+    changmin obj = new changmin(); 
+        int age = 25;
+        String group = "The Boyz";
+        char surname = 'G';
+    
+    yeri obj = new yeri(); 
+        int age = 24;
+        String group = "Red Velvet";
+        char surname = 'K';
+        
+    changmin obj = new changmin(); 
+        int age = 22;
+        String group = "NCT";
+        char surname = 'J';
+    // TODO code application logic here
+    }
+    
+}
